93f07b5a6df7ccd0c29b75da1de4263fc9b515b4
Towdog0352 <105515623+Towdog0352@users.noreply.github.com>
2026-08-25 18:52:08 -0500

    Adjust delay and modify pixel color settings

--- tkz_led_1.ino
+++ tkz_led_1.ino
@@ -21,7 +21,7 @@
 // strandtest example for more information on possible values.
 Adafruit_NeoPixel pixels(STRIPLENGTH_SHORT*2+STRIPLENGTH_LONG, PIN, NEO_GRB + NEO_KHZ800);
 
-#define DELAYVAL 15// Time (in milliseconds) to pause between pixels
+#define DELAYVAL 10// Time (in milliseconds) to pause between pixels
 
 void setup() {
   // These lines are specifically to support the Adafruit Trinket 5V 16 MHz.
@@ -33,8 +33,7 @@ void setup() {
 
   pixels.begin(); // INITIALIZE NeoPixel strip object (REQUIRED)
 }
-
-void loop() {
+void charge() {
   pixels.clear(); // Set all pixel colors to 'off'
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
@@ -44,7 +43,44 @@ void loop() {
 
     // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
     // Here we're using a moderately bright green color:
-     pixels.setPixelColor(i, pixels.Color(0, 0, 150));
+     pixels.setPixelColor(i, pixels.Color(150, 150, 150));
+     if(i>STRIPLENGTH_LONG-STRIPLENGTH_SHORT-1){
+     pixels.setPixelColor(i+STRIPLENGTH_LONG-diff, pixels.Color(150, 150, 150));
+     pixels.setPixelColor(i+STRIPLENGTH_LONG+STRIPLENGTH_SHORT-diff, pixels.Color(150, 150, 150));
+     }
+
+    pixels.show();   // Send the updated pixel colors to the hardware.
+
+    delay(DELAYVAL); // Pause before next pass through loop
+  }
+}
+
+
+void shoot() {
+  pixels.clear(); // Set all pixel colors to 'off'
+
+  // The first NeoPixel in a strand is #0, second is 1, all the way up
+  // to the count of pixels minus one
+  int diff = STRIPLENGTH_LONG-STRIPLENGTH_SHORT;
+  for(int i=0; i<STRIPLENGTH_LONG; i++) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+     pixels.setPixelColor(i, pixels.Color(150, 150, 150));
+     if(i>STRIPLENGTH_LONG-STRIPLENGTH_SHORT-1){
+     pixels.setPixelColor(i+STRIPLENGTH_LONG-diff, pixels.Color(150, 150, 150));
+     pixels.setPixelColor(i+STRIPLENGTH_LONG+STRIPLENGTH_SHORT-diff, pixels.Color(150, 150, 150));
+     }
+
+
+  }
+      pixels.show();   // Send the updated pixel colors to the hardware.
+
+  for(int i=0; i<STRIPLENGTH_LONG; i++) { // For each pixel...
+
+    // pixels.Color() takes RGB values, from 0,0,0 up to 255,255,255
+    // Here we're using a moderately bright green color:
+     pixels.setPixelColor(i, pixels.Color(150, 0, 0));
      if(i>STRIPLENGTH_LONG-STRIPLENGTH_SHORT-1){
      pixels.setPixelColor(i+STRIPLENGTH_LONG-diff, pixels.Color(150, 0, 0));
      pixels.setPixelColor(i+STRIPLENGTH_LONG+STRIPLENGTH_SHORT-diff, pixels.Color(150, 0, 0));
@@ -54,4 +90,9 @@ void loop() {
 
     delay(DELAYVAL); // Pause before next pass through loop
   }
+}
+
+
+void loop() {
+  shoot();
 }
